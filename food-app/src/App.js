3ecb3f7b01bdd18a5dd0358943f58d443b894997
moshiuzzaman <shatil4135@gmail.com>
2023-06-15 15:55:14 +0600

    Almost done

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,12 +1,18 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>    
-      <Pages></Pages>  
-      
-    </div>
+
+    <BrowserRouter>      
+      <Search></Search>
+      <Category></Category>
+      <Pages></Pages>        
+    </BrowserRouter>
   );
 }
+
+
 
 export default App;
--- food-app/src/pages/Pages.jsx
+++ food-app/src/pages/Pages.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Home from './Home'
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import Home from './Home';
+import Cuisine from './Cuisine';
+import Searched from './Searched';
+import Recipe from './Recipe';
 
 function Pages() {
   return (
-    <div>
-      <Home></Home>
-    </div>
-  )
+    
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cuisine/:type" element={<Cuisine/>}/>  
+        <Route path="/searched/:search" element={<Searched/>}></Route>
+        <Route path="/recipe/:name" element={<Recipe/>}></Route>
+      </Routes>
+    
+  );
 }
 
-export default Pages
+export default Pages;
