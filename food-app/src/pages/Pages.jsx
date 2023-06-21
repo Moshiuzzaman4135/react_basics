a2a7e404deacec50bdec57c116dbe4d2cc52c81c
moshiuzzaman <shatil4135@gmail.com>
2023-06-21 13:49:14 +0600

    Food App finished

--- food-app/src/pages/Pages.jsx
+++ food-app/src/pages/Pages.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes, useLocation} from 'react-router-dom';
 import Home from './Home';
 import Cuisine from './Cuisine';
 import Searched from './Searched';
 import Recipe from './Recipe';
+import { AnimatePresence } from 'framer-motion';
 
 function Pages() {
+  const location = useLocation();
   return (
-    
-      <Routes>
+    <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home/>}/>
         <Route path="/cuisine/:type" element={<Cuisine/>}/>  
         <Route path="/searched/:search" element={<Searched/>}></Route>
         <Route path="/recipe/:name" element={<Recipe/>}></Route>
       </Routes>
+    </AnimatePresence>
     
   );
 }
